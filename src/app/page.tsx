3c005beb90b2147a0d560f2e9c087023c4490204
fiona Vr <fiovert1192@gmail.com>
2025-01-17 09:05:13 +0100

    🚧

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,43 @@
-export default function Home() {
+import Image from "next/image";
+import axios from 'axios';
+import Link from 'next/link';
+
+interface Army {
+  title: string;
+  description: [
+    {
+      children: [
+        { text: string }
+      ]
+    }
+  ]
+  designe: [
+    {
+      url: string;
+    }
+  ]
+}
+export default async function Home() {
+  const response = await axios.get(`http://localhost:1337/api/army?populate=*`);
+  const army: Army = response.data.data;
+  console.log(response.data.data);
+
   return (
 
     <div>
-      
+      <Image
+        src={`http://localhost:1337${army.designe[0].url}`}
+        alt={army.title}
+        width={300}
+        height={300}
+        className="rounded-lg shadow-lg border-4 border-yellow-700"
+      />
+      <h1>{army.title}</h1>
+      <div>
+        {army.description.map((detail, index) => (
+          <p key={index}>{detail.children[0].text}</p>
+        ))}
+      </div>
     </div>
   );
 }
